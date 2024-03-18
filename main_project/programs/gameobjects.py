b9b9a45815f14509735ec9c4066f3e0d87165e23
LIL USER <kristoll1995@gmail.com>
2024-03-18 17:57:52 +0300

    modified:   gameobjects.py 	modified:   scripts.py 	deleted:    graphic_components.py

--- main_project/programs/gameobjects.py
+++ main_project/programs/gameobjects.py
@@ -3,4 +3,4 @@ b = 4
 result1 = a + b
 print(result1)
 result2 = a - b
-print(result2)
+print(result2)git
---
+++ main_project/programs/scripts.py
@@ -0,0 +1,6 @@
+a = 1
+b = 1
+result1 = a + b
+print(result1)
+result2 = a - b
+print(result2)
