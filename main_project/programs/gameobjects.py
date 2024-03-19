8bdf2338913ad4be0ea2a9a3fd9be684ebbdce1e
LIL USER <kristoll1995@gmail.com>
2024-03-19 13:28:47 +0300

    Добавление функции нахождение факториала

--- main_project/programs/gameobjects.py
+++ main_project/programs/gameobjects.py
@@ -2,3 +2,9 @@
 def is_leap_year(year):
     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
+#нахождение факториала
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
