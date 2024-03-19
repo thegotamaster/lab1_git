31e4158bd29a3a42c8d706cacced9cd8b7b76ce3
LIL USER <kristoll1995@gmail.com>
2024-03-19 16:21:48 +0300

    Удаление функции поиск среднего значения в списке

--- main_project/programs/scripts.py
+++ main_project/programs/scripts.py
@@ -2,11 +2,3 @@
 def is_palindrome(string):
     return string == string[::-1]
 
-#поиск среднего значения в списке
-def find_average(numbers):
-    total = sum(numbers)
-    count = len(numbers)
-    if count == 0:
-        return 0
-    else:
-        return total / count
