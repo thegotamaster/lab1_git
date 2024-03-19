b9662763ec601e0d2cee42310cd3931db580df09
LIL USER <kristoll1995@gmail.com>
2024-03-19 16:11:03 +0300

    Добавление функции вычисления площади треугольника (Герон)

--- main_project/programs/graphic_components.py
+++ main_project/programs/graphic_components.py
@@ -2,3 +2,7 @@
 def celcius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
+#вычисление площади треугольника (Герон)
+def triangle_area(a, b, c):
+    s = (a+ b+ c) / 2
+    return (s * (s - a) * (s - b) * (s - c)) ** 0.5
