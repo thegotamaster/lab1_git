dfc845a7f034610bf429ce2687bf6ac7a5af2f9c
LIL USER <kristoll1995@gmail.com>
2024-03-19 16:16:33 +0300

    Добавление функции проверка является ли строка палиндромом

--- main_project/programs/scripts.py
+++ main_project/programs/scripts.py
@@ -1,11 +1,5 @@
-<<<<<<< .merge_file_v5EKT7
-a = 1
-b = 1
-result1 = a + b
-print(result1)
-result2 = a - b
-print(result2)
-||||||| .merge_file_Hwl0Zh
-=======
-print("Hello World")
->>>>>>> .merge_file_r2C5sK
+#проверка являетсяли строка палиндромом
+def is_palindrome(string):
+    return string == string[::-1]
+
+
